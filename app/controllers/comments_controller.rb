ec88fd89e9c6a1a5c20eab588d8a2a812ebb25b5
Ng'ethe Maina <ngethe@mradi.co.ke>
2019-04-19 05:56:45 +0300

    Add comment feature
    - One is able to add his name and comment
    - Created and shown per individual article
    
    JIRA: BEDT-234

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,21 @@
+class CommentsController < ApplicationController
+
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.article_id = params[:article_id]
+
+    if @comment.save
+      redirect_to article_path(@comment.article)
+    else
+      @comment
+    end
+
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:author_name, :body)
+  end
+
+end
